490484fb093781125ecd2118bea00acff48a87ba
rajeev <rajeevbaniya11@gmail.com>
2021-05-18 09:30:16 +0545

    completed boiler plate of redux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -15,7 +18,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords donot match");
+      props.setAlert("Passwords donot match", "danger", 5000);
     } else {
       console.log("Success");
     }
@@ -81,6 +84,10 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
 
 //fuser -k -n tcp 5000
